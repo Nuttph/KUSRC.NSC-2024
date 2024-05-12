3fc92bba4c79343a18a2c32b54373ccfea349aac
Nuttaphon <NuttaphonCP2023@gmail.com>
2024-05-12 23:35:27 +0700

    debug code page

--- WEB/client/src/app/page.tsx
+++ WEB/client/src/app/page.tsx
@@ -1,9 +1,7 @@
+import React from "react";
 
+const page = () => {
+  return <div>page</div>;
+};
 
-export default function Home() {
-  return (
-    <>
-      <div>hii</div>
-    </>
-  );
-}
+export default page;
