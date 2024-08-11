bdb542c7a96fe2ded3a209f4156d19eb0a0592b1
Nuttaphon <131658916+Nuttph@users.noreply.github.com>
2024-08-11 11:27:00 +0700

    update

--- WEB/client/src/components/Topic/Header.tsx
+++ WEB/client/src/components/Topic/Header.tsx
@@ -1,23 +1,40 @@
 "use client";
 import React from "react";
 import Image from "next/image";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
-//background
-import bgKu from "../../asset/bg/bg_ku.png";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/autoplay"; // Import autoplay styles
+
+// Import required modules
+import { Pagination, Autoplay } from "swiper/modules";
+
+// Background images
+import bgKu1 from "../../asset/bg/bg_ku1.png";
+import bgKu2 from "../../asset/bg/bg_ku2.png";
+
 const Header = () => {
   return (
     <>
-      <div
-        className="relative h-[100vh] bgCustomKu w-full"
-        style={{
-          backgroundImage: `url(${bgKu})`,
-          backgroundSize: "cover", // Ensures the image covers the entire area
-          backgroundPosition: "center", // Centers the image
-          height: "95vh", // Full viewport height
-          width: "100%", // Full viewport width
-        }}
-      >
-        <div className="absolute text-[95px] text-[#fff] left-[50%] top-[35%] translate-x-[-50%] translate-y-[-50%]">
+      <div className="relative min-h-[100vh] w-full bg-red-500">
+        <Swiper
+          pagination={true}
+          modules={[Pagination, Autoplay]} // Add Autoplay module
+          className="mySwiper min-h-[100vh]"
+          loop={true}
+          autoplay={{ delay: 3000, disableOnInteraction: true }} // Autoplay with 3 seconds delay
+        >
+          <SwiperSlide>
+            <Image src={bgKu1} alt="bgku" fill className="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src={bgKu2} alt="bgku" fill />
+          </SwiperSlide>
+        </Swiper>
+        <div className="absolute text-[95px] text-[#fff] left-[50%] top-[35%] translate-x-[-50%] translate-y-[-50%] z-[100]">
           <span className="font-semibold drop-shadow-2xl">กายภาพบำบัด</span>
         </div>
       </div>
